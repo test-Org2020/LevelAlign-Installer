8951348140dca4423435ea3f8542865d7435f9c1
olga-golovkina <72074265+olga-golovkina@users.noreply.github.com>
2020-09-30 13:22:26 +0300

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,5 @@
+@echo off
+copy LevelAlign.dll  %ProgramData%\Autodesk\Revit\Addins\2019\
+copy LevelAlign.addin %ProgramData%\Autodesk\Revit\Addins\2019\
+copy LevelAlign.dll  %AppData%\Autodesk\Revit\Addins\2019\
+copy LevelAlign.addin %AppData%\Autodesk\Revit\Addins\2019\
---
+++ uninstall.bat
@@ -0,0 +1,3 @@
+@echo off
+del /q %ProgramData%\Autodesk\Revit\Addins\2019\LevelAlign.dll
+del /q %ProgramData%\Autodesk\Revit\Addins\2019\LevelAlign.addin
